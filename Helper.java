f244803b54b41188fc4b02b5e468e6432887b02c
Igor Flores Bento <81481911+IgorFlores2003@users.noreply.github.com>
2022-05-23 20:27:14 -0300

    Update Helper.java

--- Helper.java
+++ Helper.java
@@ -1,72 +1,72 @@
 package Helper;
+
 import java.io.*;
 import java.util.Scanner;
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 
-
-
-public class Help {
+public class Help_2 {
     public void CreateFiles(String Tex, Scanner x) {
-        File dir = new File("C:\\TrabalhoProgramacao");
-        File arq = new File(dir, Tex);
+        File dir = new File(TEST.New_Information(TEST.CSV_FILE));
+        File arq = new File(dir, Tex +".txt");
+        int choose;
+        int aux=0;
+        String read= TEST.CSV_FILE;
+
+        try {
+            FileWriter File = new FileWriter(TEST.New_Information(TEST.CSV_FILE) + Tex + ".txt");
+            PrintWriter Print = new PrintWriter(arq);
+
+            do {
+                System.out.println("Informe o(s) nome(s) do(s) Aluno(s), por gentileza:");
+                String Edi = x.nextLine();
+                Print.print(Edi);
+                System.out.println("Deseja cadastrar mais alunos a chamada?\n1-Sim\n2-Não");
+                choose = x.nextInt();
+                if (choose == 1) {
+                     Edi = x.nextLine();
+                    Print.println(Edi);
+                } else if (choose == 2) {
+                    aux++;
+
+                }
+            } while (aux == 0) ;
+
+                File.close();
+                Print.close();
+            }catch(IOException e){
+
+                e.printStackTrace();
+            }
+        Reader(read);
+        }
+
+    public void Reader (String arqui){
+        File dir = new File(TEST.CSV_FILE);
+        File arq = new File(dir, arqui);
+        System.out.println("Informações Salvas!!!");
+        System.out.println("Nome do arquivo:" + arq);
+        System.out.println("Informações Salvas no Arquivo:" + arqui + "\n");
 
         try {
 
-            arq.createNewFile();
+            FileReader Reader = new FileReader(arqui);
 
-            FileWriter File = new FileWriter(Tex, false);
+            BufferedReader bufferedReader = new BufferedReader(Reader);
 
-            PrintWriter Print = new PrintWriter(arq);
-            String Edi = x.nextLine();
+            String linha = "";
 
-                Print.println(Edi);
-                Print.flush();
-                Print.close();
+            while ((linha = bufferedReader.readLine()) != null) {
+                System.out.println(linha);
 
-
+            }
+            Reader.close();
+            bufferedReader.close();
         } catch (IOException e) {
-
-                e.printStackTrace();
-            }
+            e.printStackTrace();
         }
 
-        public void MENU () {
-
-            System.out.println("<<<<<<EXEMPLO DE COMO DEVE SER CADASTRADA A CHAMADA>>>>>>\n [matematica_3_14_05_2022]");
-            System.out.println("-----------CHAMADA-----------");
-            System.out.println("1-Matemática");
-            System.out.println("2-Educação Física");
-            System.out.println("3-Português");
-            System.out.println("0-Sair");
-            System.out.println("-----------------------------");
-        }
-        public void Reader (String arqui){
-            File dir = new File("C:\\TrabalhoProgramacao");
-            File arq = new File(dir, arqui);
-            System.out.println("Informações Salvas!!!");
-            System.out.println("Nome do arquivo:" + arq);
-            System.out.println("Informações Salvas no Arquivo:" + arqui + "\n");
-
-            try {
-
-                FileReader Reader = new FileReader(arq);
-
-                BufferedReader bufferedReader = new BufferedReader(Reader);
-
-                String linha = "";
-
-                while ((linha = bufferedReader.readLine()) != null) {
-                    System.out.println(linha);
-
-                }
-                Reader.close();
-                bufferedReader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }}
+    }}
 
 
